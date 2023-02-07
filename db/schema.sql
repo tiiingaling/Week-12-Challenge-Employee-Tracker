c52d3c14c54ab9756f459eb480645bebe66c3633
tiiingaling <93777306+tiiingaling@users.noreply.github.com>
2023-02-07 18:57:11 +0000

    filled in table parameters

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,25 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department
+{
+    -- AUTO_INCREMENT automatically generates a number with a new record
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+};
 
 CREATE TABLE role
+{
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+};
 
 CREATE TABLE employee
+{
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+};
