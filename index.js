c6c9970f79b8aae690157fb5ab765a0f5f6eba51
tiiingaling <93777306+tiiingaling@users.noreply.github.com>
2023-02-10 15:00:24 +0000

    added code for viewing data

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 const inquirer = require('inquirer');
 const EmployeeDatabase = require('./db/EmployeeDatabase');
-const sequelize = require('./config/connection');
+const db = require('./config/connection');
 
+
+// connects to the database
+db.connect();
+
+// to do:
+// view employees
+// view roles
+// view department
+// add employee
+// add role
+// add department
+
+const view_employees = () => {
+
+    db.getDepartment().then((results) => {
+
+        console.table(results);
+    })
+}
+
+const view_roles = () => {
+    
+    db.getRole().then((results) => {
+
+        console.table(results);
+    })
+}
+
+const view_departments = () => {
+    
+    db.getDepartment().then((results) => {
+
+        console.table(results);
+    })
+}
